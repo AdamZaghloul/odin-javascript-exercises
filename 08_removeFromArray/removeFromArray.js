5198bdb2b9b2071a81969b82223bc4c2d67d9fed
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-07-24 19:51:39 -0400

    javascript problem 8.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...params) {
+    for(const param of params){
+        array = array.filter(item => {
+            return item === param ? false : true;
+        });
+    }
+
+    return array;
 };
 
 // Do not edit below this line

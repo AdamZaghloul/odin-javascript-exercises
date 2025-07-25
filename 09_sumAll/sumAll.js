f90b1c49d0307bf4b0d3f970e03933b25202b3b3
AdamZaghloul <adam.zaghloul95@gmail.com>
2025-07-24 20:00:06 -0400

    javascript problem 9.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let larger = 0;
+    let smaller = 0;
 
+    if(a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+    if(a>b){
+        larger = a;
+        smaller = b;
+    }else if(b > a){
+        larger = b;
+        smaller = a;
+    }else{
+        return a+b;
+    }
+
+    let sum = a + b;
+
+    for(let i = smaller+1; i < larger; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
